190768b7e92bc4dec7ea2326f04ddcef190aa025
Cevdet Gulser <c.gulser86@gmail.com>
2023-01-10 11:50:52 +0100

    10.01.2023

--- cypress/e2e/day02/03_dropDown.cy.js
+++ cypress/e2e/day02/03_dropDown.cy.js
@@ -5,7 +5,7 @@ describe('dropdown',()=>{
         cy.visit('https://the-internet.herokuapp.com/dropdown')
         
         cy.get('#dropdown').select('Option 1').should('have.value','1')
-        
+        cy.get('#dropdown').select('Option 2')
 
 
     })
